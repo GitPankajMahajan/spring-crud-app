c7ed74bc01ffd0de59813cabae9c1e4d7880cca6
Pankaj Mahajan <133773048+GitPankajMahajan@users.noreply.github.com>
2023-05-16 14:54:00 +0100

    Add files via upload

--- src/main/java/com/pdp/springcrudapp/PdpAppController.java
+++ src/main/java/com/pdp/springcrudapp/PdpAppController.java
@@ -1,1 +1,14 @@
+package com.pdp.springcrudapp;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PdpAppController {
+
+	@RequestMapping("/policyType")
+	public String getPolicyType() {
+		return "Car";
+	}
+	
+}
